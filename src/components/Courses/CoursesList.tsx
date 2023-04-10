eba8fbe633395626b46bbe26043ce3f1820aedf1
Elina Kolodiazhna <EK095687@cerner.net>
2023-04-11 00:14:38 +0200

    prettier

--- src/components/Courses/CoursesList.tsx
+++ src/components/Courses/CoursesList.tsx
@@ -49,8 +49,7 @@ function CoursesList() {
 			return {
 				...course,
 				authors: course.authors.map(
-					(authorId) =>
-						authors.find((author) => author.id === authorId).name
+					(authorId) => authors.find((author) => author.id === authorId).name
 				),
 			};
 		});
