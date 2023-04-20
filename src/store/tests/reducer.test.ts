b682c47c2f353ea3f87a31f4e76268e61bf1da61
Elina Kolodiazhna <EK095687@cerner.net>
2023-04-20 16:19:09 +0200

    test create course reducer

--- src/store/tests/reducer.test.ts
+++ src/store/tests/reducer.test.ts
@@ -1,6 +1,9 @@
 import coursesReducer from '../courses/coursesSlice';
 import usersReducer from '../users/usersSlice';
 import authorsReducer from '../authors/authorsSlice';
+import { createCourse } from 'store/courses/thunk';
+
+import { newCourse } from 'components/Courses/Course.types';
 
 const initialState = { entities: [], loading: false, error: '' };
 const usersInitialState = {
@@ -10,13 +13,38 @@ const usersInitialState = {
 	},
 	error: '',
 };
+const course: newCourse = {
+	title: 'title',
+	description: 'description',
+	duration: 30,
+	authors: ['author1_id', 'author2_id'],
+};
 
-test('coursesReducer should return the initial state', () => {
-	expect(coursesReducer(undefined, { type: undefined })).toEqual(initialState);
+describe('create course', () => {
+	test('coursesReducer should return the initial state', () => {
+		expect(coursesReducer(undefined, { type: undefined })).toEqual(
+			initialState
+		);
+	});
+
+	test('add created course', () => {
+		const action = {
+			type: createCourse.fulfilled.type,
+			payload: course,
+		};
+		const result = coursesReducer(initialState, action);
+		expect(result).toEqual({
+			entities: [course],
+			loading: false,
+			error: '',
+		});
+	});
 });
 
 test('authorsReducer should return the initial state', () => {
-	expect(authorsReducer(undefined, { type: undefined })).toEqual(initialState);
+	expect(authorsReducer(undefined, { type: undefined })).toEqual(
+		initialState
+	);
 });
 
 test('usersReducer should return the initial state', () => {
